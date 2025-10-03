da702a24fea97602c25248de8cca5ea2d828d1aa
jenous11 <jenousdongol11@gmail.com>
2025-10-03 21:13:15 +0545

    added some bootstrap for style

--- createoptions.php
+++ createoptions.php
@@ -5,26 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+
 </head>
 
 <body>
-    <p>work on how to not insert empty data</p>
-    <form action="includes/insert.php" method="post">
-        Name of the player:
-        <input type="text" name="name" placeholder="enter the name of the player" required>
-        <br>
-        <br>
-        Name of the club:
-        <input type="text" name="club" placeholder="enter the name of the club">
-        <br>
-        <br>
-        Name of the position:
-        <input type="text" name="position" placeholder="enter the postion of play">
-        <br>
-        <br>
-        <input type="submit" name="submit">
-    </form>
-  
+    <h1 class="text-sm fs mt-4 fs-3 ">Enter Player Details</h1>
+    <div class="d-flex mt-5  justify-content-center ">
+        <form action="insert.php" method="post">
+            <div class="mb-3">
+                <label for="pname" class="form-label">pname</label>
+                <input type="text" class="form-control form-control-lg" name="name" ">
+  </div>
+  <div class=" mb-3">
+                <label for="club" class="form-label">club</label>
+                <input type="text" class="form-control " name="club">
+            </div>
+            <div class="mb-3 ">
+                <label class="form-label" for="position">Position</label>
+                <input type="text" class="form-control" name="position">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+    </div>
+
 </body>
 
 </html>
